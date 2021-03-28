403d462680db09065574815ec86a8df668994f33
Erling Erlingsson <erlingede@gmail.com>
2021-03-28 06:28:57 +0000

    Add axios default header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import type { AppProps } from 'next/app'
+import axios from 'axios'
 
 import 'bulma-pro/css/bulma.css'
 import 'react-toastify/dist/ReactToastify.css'
+
+axios.defaults.headers = {
+  'x-api-key': process.env.NEXT_PUBLIC_API_KEY,
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
