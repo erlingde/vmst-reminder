bcb09e3fed62942031300516173d1ffdaec7454c
Erling Erlingsson <erlingede@gmail.com>
2021-04-28 20:30:33 +0000

    Refactor styles to styled-components

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable no-nested-ternary */
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSpinner, faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import styled, { keyframes, css } from 'styled-components'
 
 type Props = {
   onClick: () => void
@@ -10,21 +12,98 @@ type Props = {
   success: boolean
 }
 
-const Button = ({ onClick, disabled, isLoading, title, success }: Props) => (
-  <button
-    className={`btn ${success ? 'btn-disabled' : ''}`}
-    type="button"
-    disabled={disabled}
-    onClick={() => onClick()}
-  >
+type FAIconProps = {
+  className: string
+  spinner?: boolean
+  icon: IconProp
+}
+
+const Button = styled.button`
+  position: relative;
+  display: block;
+  margin: 2rem auto 0 auto;
+  width: 100%;
+  max-width: 10rem;
+  height: 3rem;
+  border-radius: 5px;
+  cursor: pointer;
+  background: none;
+  overflow: hidden;
+  border: 1px solid ${(props) => props.theme.colors.primary};
+  color: ${(props) => props.theme.colors.primary};
+  transition: color 0.8s;
+  z-index: 1;
+
+  &:hover,
+  &:focus {
+    color: white;
+
+    &:before,
+    &:before {
+      transform: scaleX(1);
+      transform-origin: left;
+    }
+  }
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-color: ${(props) => props.theme.colors.primary};
+    z-index: -1;
+    transition: transform 0.8s;
+    transform: scaleX(0);
+    transform-origin: right;
+    will-change: transform;
+    transform: ${(props) => props.disabled && 'scaleX(1)'};
+    transform-origin: ${(props) => props.disabled && 'left'};
+  }
+
+  cursor: ${(props) => props.disabled && 'not-allowed'};
+  color: ${(props) => props.disabled && 'white'};
+`
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`
+const FAIcon = ({ className, icon }: FAIconProps) => (
+  <FontAwesomeIcon icon={icon} className={className} />
+)
+
+const StyledFAIcon = styled(FAIcon)<FAIconProps>`
+  width: 2rem;
+  animation: ${(props) =>
+    props.spinner &&
+    css`
+      animation: ${rotate} 1.5s linear infinite;
+    `};
+`
+
+const SubscribeButton = ({
+  onClick,
+  disabled,
+  isLoading,
+  title,
+  success,
+}: Props) => (
+  <Button type="button" disabled={disabled} onClick={() => onClick()}>
     {success ? (
-      <FontAwesomeIcon icon={faCheckCircle} className="btn-icon" />
+      <StyledFAIcon icon={faCheckCircle} className="" />
     ) : isLoading ? (
-      <FontAwesomeIcon icon={faSpinner} className="btn-icon btn-spinner" />
+      <StyledFAIcon icon={faSpinner} className="" spinner />
     ) : (
       title
     )}
-  </button>
+  </Button>
 )
 
-export default Button
+export default SubscribeButton
