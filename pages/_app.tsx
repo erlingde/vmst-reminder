ecdf36034aa06953ae035a149798a3d53df89677
Erling Erlingsson <erlingede@gmail.com>
2021-05-06 02:35:42 +0000

    Refactor theme and global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { NextSeo } from 'next-seo'
 import axios from 'axios'
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
@@ -6,54 +7,26 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import 'react-toastify/dist/ReactToastify.css'
 import './index.css'
 
+import { globalStyles, theme } from 'theme'
+
 axios.defaults.headers = {
   'x-api-key': process.env.NEXT_PUBLIC_API_KEY,
 }
 
-const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-
-  body {
-    font-family: 'Poppins', sans-serif !important;
-    color: #333;
-    line-height: 1.6;
-  }
-
-  html { 
-    background-image: url("data:image/svg+xml,%3Csvg width='6' height='6' viewBox='0 0 6 6' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%239C92AC' fill-opacity='0.4' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 6V5zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E");
-  }
-
-  h1 {
-    font-size: clamp(40px, 5vw, 70px);
-  }
-
-  h1,
-  h2 {
-    font-weight: 300;
-    line-height: 1.2;
-    margin: 10px 0;
-  }
-
-  p {
-    font-size: clamp(1em, 2vw, 2em);
-    font-weight: 400;
-    margin: 10px 0;
-  }
-`
-
-const theme = {
-  colors: {
-    primary: '#3498db',
-  },
-}
+const GlobalStyle = createGlobalStyle`${globalStyles}`
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <NextSeo
         title="VMST Reminder"
         description="Get a monthly reminder when to verify job search."
---
+++ theme/index.ts
@@ -0,0 +1,59 @@
+export const globalStyles = `
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Poppins', sans-serif !important;
+    color: #333;
+    line-height: 1.6;
+  }
+
+  html { 
+    background-image: url("data:image/svg+xml,%3Csvg width='6' height='6' viewBox='0 0 6 6' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%239C92AC' fill-opacity='0.4' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 6V5zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E");
+  }
+
+  h1 {
+    font-size: clamp(40px, 5vw, 70px);
+  }
+
+  h1,
+  h2 {
+    font-weight: 300;
+    line-height: 1.2;
+    margin: 10px 0;
+  }
+
+  p {
+    font-size: clamp(1em, 2vw, 2em);
+    font-weight: 400;
+    margin: 10px 0;
+  }
+
+  a, a:visited {
+    text-decoration: none;
+    color: #333;
+  }
+
+  @media (prefers-reduced-motion: reduce) {
+    *, ::before, ::after {
+      animation-delay: -1ms !important;
+      animation-duration: 1ms !important;
+      animation-iteration-count: 1 !important;
+      background-attachment: initial !important;
+      scroll-behavior: auto !important;
+      transition-duration: 0s !important;
+      transition-delay: 0s !important;
+    }
+  }
+`
+
+export const theme = {
+  colors: {
+    primary: '#3498db',
+  },
+}
