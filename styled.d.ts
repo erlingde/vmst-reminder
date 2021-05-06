a3039794ac0fc887719a3be1554aac15affabbf5
Erling Erlingsson <erlingede@gmail.com>
2021-05-06 05:59:25 +0000

    Remove interface

--- styled.d.ts
+++ styled.d.ts
@@ -1,10 +1,5 @@
 import 'styled-components'
 
-interface IPalette {
-  main: string
-  contrastText: string
-}
-
 declare module 'styled-components' {
   export interface DefaultTheme {
     colors: {
