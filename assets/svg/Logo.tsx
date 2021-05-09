79f37455bf11ed71fd6e10ce5f8a8828fba5d779
Erling Erlingsson <erlingede@gmail.com>
2021-05-09 04:52:41 +0000

    Add logo animation on hover

--- assets/svg/Logo.tsx
+++ assets/svg/Logo.tsx
@@ -1,11 +1,37 @@
+import styled, { keyframes } from 'styled-components'
+
 type Props = {
   className: string
   width: string
   height: string
 }
 
+const Alert = keyframes`
+  from {
+    transform: translateY(0)
+  }
+
+  50% {
+    transform: translateY(-2px)
+  }
+
+  to {
+    transform: translateY(0)
+  }
+`
+
+const StyledLogo = styled.svg`
+  overflow: visible;
+  &:hover #RightLine,
+  &:focus #RightLine,
+  &:hover #LeftLine,
+  &:focus #LeftLine {
+    animation: ${Alert} 0.5s linear;
+  }
+`
+
 const Logo = ({ className, width, height }: Props) => (
-  <svg
+  <StyledLogo
     className={className}
     width={width}
     height={height}
@@ -19,9 +45,9 @@ const Logo = ({ className, width, height }: Props) => (
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
     <path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" />
     <path d="M9 17v1a3 3 0 0 0 6 0v-1" />
-    <path d="M21 6.727a11.05 11.05 0 0 0 -2.794 -3.727" />
-    <path d="M3 6.727a11.05 11.05 0 0 1 2.792 -3.727" />
-  </svg>
+    <path id="RightLine" d="M21 6.727a11.05 11.05 0 0 0 -2.794 -3.727" />
+    <path id="LeftLine" d="M3 6.727a11.05 11.05 0 0 1 2.792 -3.727" />
+  </StyledLogo>
 )
 
 export default Logo
