350df643bda7094df090914c67382d7544403977
Erling Erlingsson <erlingede@gmail.com>
2021-05-23 18:46:39 +0000

    Refactor component rendering hierarchy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,37 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { NextSeo } from 'next-seo'
 import axios from 'axios'
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { ToastContainer } from 'react-toastify'
+
+import Header from 'components/molecules/Header'
+
+import Logo from 'assets/svg/Logo'
 
 import 'react-toastify/dist/ReactToastify.css'
-import './index.css'
 
 import { globalStyles, theme } from 'theme'
+
+const Main = styled.main`
+  /* filter: invert(1) hue-rotate(180deg); */
+  position: relative;
+  overflow: hidden;
+  min-height: 100%;
+  overflow: visible;
+
+  // Dark theme
+  /* &:before {
+    content: '';
+    display: block;
+    position: absolute;
+    width: 50px;
+    height: 50px; */
+  /* background-color: ${(props) => props.theme.colors.secondary}; */
+  /* filter: invert(1) hue-rotate(180deg);
+    border-radius: 50%;
+    animation:  1s ease forwards;
+  } */
+`
 
 axios.defaults.headers = {
   'x-api-key': process.env.NEXT_PUBLIC_API_KEY,
@@ -23,7 +48,7 @@ function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;800&display=swap"
           rel="stylesheet"
         />
       </Head>
@@ -33,8 +58,16 @@ function App({ Component, pageProps }: AppProps) {
       />
       <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <Header>
+          <a href="/">
+            <Logo width="60" height="60" color={theme.colors.primary} />
+          </a>
+        </Header>
+        <Main>
+          <Component {...pageProps} />
+        </Main>
       </ThemeProvider>
+      <ToastContainer />
     </>
   )
 }
