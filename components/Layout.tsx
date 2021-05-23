8f613adc64b90a1fff9ddbeefbcfa2fd8408818e
Erling Erlingsson <erlingede@gmail.com>
2021-05-23 18:47:48 +0000

    Refactor layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,60 +1,9 @@
-import { ReactChild, forwardRef } from 'react'
-import Link from 'next/link'
-
-import styled from 'styled-components'
-
-import Logo from 'assets/svg/Logo'
+import { ReactNode } from 'react'
 
 type Props = {
-  children: ReactChild
+  children: ReactNode
 }
 
-type StyledLogoProps = {
-  className: string
-  width: string
-  height: string
-  href?: string
-}
-
-const Container = styled.div`
-  max-width: 1200px;
-  min-height: 100vh;
-  margin: 0 auto;
-  padding: 0 40px;
-  display: flex;
-  flex-direction: column;
-`
-
-const Header = styled.header`
-  padding-top: 2rem;
-  position: sticky;
-  justify-content: space-between;
-  align-items: center;
-  display: flex;
-  flex: 0 0 100px;
-`
-
-const RenderLogo = forwardRef<HTMLAnchorElement, StyledLogoProps>(
-  ({ className, href }, ref) => (
-    <a href={href} ref={ref}>
-      <Logo className={className} width="60" height="60" />
-    </a>
-  )
-)
-
-const StyledLogo = styled(RenderLogo)<StyledLogoProps>`
-  stroke: ${(props) => props.theme.colors.primary};
-`
-
-const Layout = ({ children }: Props) => (
-  <Container>
-    <Header>
-      <Link href="/" passHref>
-        <StyledLogo className="" width="60" height="60" />
-      </Link>
-    </Header>
-    {children}
-  </Container>
-)
+const Layout = ({ children }: Props) => <>{children}</>
 
 export default Layout
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,69 +1,37 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import Card from 'components/organisms/Card'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
-import { ToastContainer, toast } from 'react-toastify'
+import Container from 'components/atoms/Container'
+import Heading from 'components/atoms/Heading'
+import Paragraph from 'components/atoms/Paragraph'
+import TextLink from 'components/atoms/TextLink'
+import Section from 'components/atoms/Section'
+import Animate from 'components/atoms/Animate'
+import { slideInFromRight, slideInFromLeft } from 'components/atoms/Keyframes'
 
-import Form from 'components/molecules/Form'
+import SubscribeForm from 'components/molecules/SubscribeForm'
+import SubscribeSuccess from 'components/molecules/SubscribeSuccess'
 
-const Home = (): JSX.Element => {
-  const displayToast = {
-    error: (label: string) => toast.error(label),
-    success: (label: string) => toast.success(label),
-  }
-
-  return (
-    <>
-      <Head>
-        <title>VMST Reminder</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <header>
-        <div className="container flex">
-          <Link href="/">
-            <Image
-              src="/Logo.png"
-              alt="Logo"
-              width="100"
-              height="74"
-              className="header-item"
+const Home = (): JSX.Element => (
+  <>
+    <Container>
+      <Section>
+        <Animate animation={slideInFromLeft}>
+          <Heading primary="Sign up." secondary="Enjoy" />
+          <Paragraph>
+            Sign up for a monthly e-mail reminder to verify job search on{' '}
+            <TextLink
+              href="https://www.vinnumalastofnun.is"
+              label="Vinnumálastofnun!"
             />
-          </Link>
-          <Link href="https://github.com/erlingde/vmst-reminder">
-            <FontAwesomeIcon icon={faGithub} className="header-item github" />
-          </Link>
-        </div>
-      </header>
-      <main>
-        <div className="container grid">
-          <div className="information">
-            <h1>Sign up.</h1>
-            <h1>
-              Enjoy<span className="information--dot">.</span>
-            </h1>
-            <p>
-              Sign up for a monthly e-mail reminder to verify job search on
-              Vinnumálastofnun!
-            </p>
-          </div>
-          <Form displayToast={displayToast} />
-        </div>
-      </main>
-      <ToastContainer />
-      <div className="email-icon-wrapper">
-        <Image src="/email.png" width="300px" height="300px" />
-      </div>
-      {/* <div className="box"></div> */}
-    </>
-  )
-}
+          </Paragraph>
+        </Animate>
+        <Animate animation={slideInFromRight}>
+          <Card front={<SubscribeForm />} back={<SubscribeSuccess />} />
+        </Animate>
+      </Section>
+    </Container>
+  </>
+)
 
 export default Home
