80332082f044beb6397e2ac314be8747ecc1f861
Erling Erlingsson <erlingede@gmail.com>
2021-05-25 15:29:38 +0000

    Refactor email templates

--- email/reminder.ts
+++ email/reminder.ts
@@ -15,9 +15,9 @@ export default `
         </mj-section>
         <mj-section background-color="#00765f" border-top="1px solid #00765f">
             <mj-column width="400px">
-            <mj-divider border-width="1px" border-style="solid" border-color="lightgrey"  padding-top='35px' />
-            <mj-text font-style="italic" font-size="15px" font-family="Poppins, Helvetica Neue" color='white'>Don't want to see these emails anymore?</mj-text>
-            <mj-button background-color="orange" href="{{host}}/unsubscribe/{{id}}" font-family="Poppins, Helvetica Neue">Unsubscribe</mj-button>
+                <mj-divider border-width="1px" border-style="solid" border-color="lightgrey"  padding-top='35px' />
+                <mj-text font-style="italic" font-size="15px" font-family="Poppins, Helvetica Neue" color='white'>Don't want to see these emails anymore?</mj-text>
+                <mj-button background-color="orange" href="{{host}}/unsubscribe/{{id}}" font-family="Poppins, Helvetica Neue">Unsubscribe</mj-button>
             </mj-column>
         </mj-section>
     </mj-body>
--- email/verify.ts
+++ email/verify.ts
@@ -7,11 +7,11 @@ export default `
     <mj-body>
         <mj-section background-color="#00765f">
             <mj-column width="400px">
-            <mj-image width="50px" src="https://i.ibb.co/cr85xNd/Logo-white.png" />
-            <mj-text font-style="italic" font-size="20px" font-family="Poppins, Helvetica Neue" color="white">Hey you! Please confirm your e-mail address below</mj-text>
+                <mj-image width="50px" src="https://i.ibb.co/cr85xNd/Logo-white.png" />
+                <mj-text font-style="italic" font-size="20px" font-family="Poppins, Helvetica Neue" color="white">Hey you! Please confirm your e-mail address below</mj-text>
 
-            <mj-button href="{{host}}/verify/{{id}}" background-color="white" color="#00765f" font-family="Poppins, Helvetica Neue" padding-top='35px'>Verify E-Mail</mj-button>
-                    <mj-text color="white" padding-top="50px" font-family="Poppins, Helvetica Neue">If you received this e-mail by mistake, simply delete it. You won't be subscribed if you don't click on the confirmation link above.</mj-text>
+                <mj-button href="{{host}}/verify/{{id}}" background-color="white" color="#00765f" font-family="Poppins, Helvetica Neue" padding-top='35px'>Verify E-mail</mj-button>
+                <mj-text color="white" padding-top="50px" font-family="Poppins, Helvetica Neue">If you received this e-mail by mistake, simply delete it. You won't be subscribed if you don't click on the confirmation link above.</mj-text>
             </mj-column>
         </mj-section>
     </mj-body>
